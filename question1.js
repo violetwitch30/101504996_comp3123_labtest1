d121fe94e93a65439488461f6eb67c644f798b15
Fidan Zeynalli <fidan.zeynalli@georgebrown.ca>
2025-10-02 14:09:22 -0400

    question 1

--- question1.js
+++ question1.js
@@ -15,7 +15,3 @@ function lowerCaseWords(mixedArray) {
 lowerCaseWords(["PIZZA", 10, true, 25, false, "Wings"])
     .then(result => console.log(result))
     .catch(error => console.error(error));
-
-lowerCaseWords()
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
